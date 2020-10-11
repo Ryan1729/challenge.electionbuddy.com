80de72962c10759be2788cec83fe6d9e84831b02
Ryan1729 <Ryan1729@gmail.com>
2020-10-10 20:24:14 -0600

    log all changes to elections

--- app/models/election.rb
+++ app/models/election.rb
@@ -2,22 +2,29 @@ class Election < ApplicationRecord
   has_many :questions
   has_many :audit_events, dependent: :destroy
   belongs_to :user
+  before_save :log_changed
 
   serialize :settings, Hash
+
+  def log_changed
+    if self.changed?
+      self.changed.each do |attr|
+        audit_events.create(
+          :user_id=>user.id,
+          :column_name=>attr.to_s,
+          :table_name=>"elections",
+          :old_value=>self.attributes[attr],
+          :new_value=>self.changed_attributes[attr]
+        )
+      end
+    end
+  end
 
   def visibility
     settings[:visibility]
   end
 
   def visibility=(value)
-    audit_events.create(
-      :user_id=>user_id,
-      :column_name=>"settings.visibility",
-      :table_name=>"elections",
-      :old_value=>visibility,
-      :new_value=>value
-    )
-  
     settings[:visibility] = value
   end
 end
