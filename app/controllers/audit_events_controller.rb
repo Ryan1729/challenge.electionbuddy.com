bafbae781a638501794fda76ff091c5b4944ff74
Ryan1729 <Ryan1729@gmail.com>
2020-10-10 18:49:24 -0600

    list audit events on audit page

--- app/controllers/audit_events_controller.rb
+++ app/controllers/audit_events_controller.rb
@@ -1,5 +1,12 @@
 class AuditEventsController < ApplicationController
-    def index
+    before_action :get_election
 
+    def index
+        @audit_events = @election.audit_events
     end
+
+    private
+        def get_election
+            @election = Election.find(params[:election_id])
+        end
 end
